745deda319b69ffd47ebb8d087abd1a3160cdd14
puja <deshmukhpooja976@gmail.com>
2022-07-23 20:18:49 +0530

    add

--- cart.js
+++ cart.js
@@ -1,6 +1,6 @@
-var cartItemsArr = JSON.parse(localStorage.getItem("cart_data")) || [];
+var cartItems = JSON.parse(localStorage.getItem("cart_data")) || [];
 
-if (cartItemsArr.length == 0) {
+if (cartItems.length == 0) {
   let div = document.createElement("div");
   let img = document.createElement("img");
   img.src = "images/empty-cart-icon.svg";
@@ -21,71 +21,91 @@ if (cartItemsArr.length == 0) {
   let a = document.getElementById("container");
   a.innerHTML = "";
   a.append(div);
-} else {
+} 
+else {
   let a = document.getElementById("cart_items");
   let c = document.getElementById("cart_total");
 
   let total_price = 0;
   let total_discount = 0;
   let total_d = 0;
-  function displayArr(cartItemsArr) {
-    cartItemsArr.map((element) => {
+  function displayArr(cartItems) {
+    cartItems.map((element,index) => {
       let count = 1;
       let b = document.createElement("div");
       b.style.display = "flex";
-      let { name, quantity, mrp, price } = element;
+      let { mrp, price } = element;
       let temp = document.createElement("div");
       let temp1 = document.createElement("div");
       temp.style.width = "50%";
       temp1.style.width = "50%";
       temp1.style.textAlign = "right";
       let h2 = document.createElement("h3");
+      h2.innerText = element.name;
+
       let price_h2 = document.createElement("h3");
+      price_h2.innerText = `Rs. ${price}`;
+
       let org_price = document.createElement("h5");
       org_price.id = "dsc_prc";
-      let cnt = document.createElement("span");
+      org_price.innerText = `Rs. ${mrp}`;
+
+    
+       let cnt = document.createElement("span");
+
+       cnt.innerText="1"
+      cnt.id="cnt"
       let qty = document.createElement("p");
+      qty.innerText = element.quantity;
+
       let plus = document.createElement("button");
-      let minus = document.createElement("button");
-      let del = document.createElement("button");
-      del.addEventListener("click", function (index) {
-        cartItemsArr.splice(index, 1);
-        // count--;
-        localStorage.setItem("cart_data", JSON.stringify(cartItemsArr));
-        displayArr(cartItemsArr);
-        location.reload();
-      });
+      plus.innerText = "+";
+      plus.id = "plus";//for style
       plus.addEventListener("click", function () {
-        let arr = JSON.parse(localStorage.getItem("cart_data"));
-        count++;
-        price_h2.innerText = `Rs. ${+price * count}`;
+      count++;
+        price_h2.innerText = `Rs. ${+price  * count}`;
         cnt.innerText = count;
         total_price = +price * count;
         total_discount = +mrp * count;
-        // displayArr(cartItemsArr);
+      
         location.reload;
       });
-
-      plus.innerText = "+";
+      let minus = document.createElement("button");
       minus.innerText = "-";
-    
-      plus.id = "plus";
-      minus.id = "minus";
-      del.id = "del";
-      del.innerText = "Remove";
-      price_h2.innerText = `Rs. ${price}`;
-      org_price.innerText = `Rs. ${mrp}`;
-      h2.innerText = name;
-      qty.innerText = quantity;
-      temp.append(h2, qty, del);
+       minus.id = "minus";
+       minus.addEventListener("click", function () {
+       
+        count--;
+        price_h2.innerText = `Rs. ${price * count}`;
+        cnt.innerText = count;
+        total_price = price * count;
+        total_discount = mrp * count;
+        location.reload;
 
-      temp1.append(price_h2, org_price,minus,plus);
+      });
+
+    let del = document.createElement("button");
+      del.innerText = "Remove";
+      del.id = "del";
+
+      del.addEventListener("click", function (index) {
+        cartItems.splice(index, 1);
+        // count--;
+        localStorage.setItem("cart_data", JSON.stringify(cartItems));
+        displayArr(cartItems);
+        location.reload();
+      });
+      
+      
+    temp.append(h2, qty, del);
+
+      temp1.append(price_h2, org_price,minus,cnt,plus);
       b.append(temp, temp1);
       a.append(b);
 
 
-      total_price += +mrp;
-      total_discount += +price;
+       total_price += +mrp;
+       total_discount += +price;
     });
     total_d = total_discount + 10;
     document.getElementById("itm_total").innerText = total_price;
@@ -100,6 +120,9 @@ if (cartItemsArr.length == 0) {
   }
 }
 
+
+
+displayArr(cartItems);
 document.getElementById("chkout").addEventListener("click", function () {
   let user_login = JSON.parse(localStorage.getItem("user_login"));
   if(user_login==true){
@@ -116,5 +139,3 @@ document.querySelector("#mg_home").addEventListener("click", () => {
 document.querySelector("#need_help").addEventListener("click", () => {
   window.location.href = "NeedHelp.html";
 });
-
-displayArr(cartItemsArr);
